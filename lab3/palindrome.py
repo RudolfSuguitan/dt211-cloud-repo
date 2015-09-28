7341edb8e1c7d7461d194d4dd9908f7b536fc351
RudolfSuguitan <C13460538@mydit.ie>
2015-09-28 20:52:52 +0000

    Code

---
+++ lab3/palindrome.py
@@ -0,0 +1,18 @@
+string[0] = "Oxo";
+string[1] = "OXO";
+string[2] = "123454321";
+string[3] = "ROTATOR";
+string[4] = "12345 54321";
+
+i=0;
+while i<5:
+  string[i] = string[i].casefold();
+  rev_string = reversed(string[i]);
+
+  if list(string[i] == rev_string):
+    print("True")
+  else
+    print("False")
+
+  i+=1
+
